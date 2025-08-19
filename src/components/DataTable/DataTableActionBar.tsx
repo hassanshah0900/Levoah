2279484e574c7e29a48ab36cb93d0374c24678c8
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-19 19:44:18 +0500

    feat: create DataTableActionBar component

---
+++ src/components/DataTable/DataTableActionBar.tsx
@@ -0,0 +1,95 @@
+import { cn } from "@/lib/utils";
+import { Table } from "@tanstack/react-table";
+import { X } from "lucide-react";
+import { ComponentProps, ReactNode } from "react";
+import { Button } from "../ui/button";
+import { Separator } from "../ui/separator";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+
+interface Props<TData> {
+  table: Table<TData>;
+  children?: ReactNode;
+}
+
+export default function DataTableActionBar<TData>({
+  table,
+  children,
+}: Props<TData>) {
+  return (
+    <div
+      className={cn(
+        "fixed bottom-0 left-1/2 -translate-x-1/2 border border-border shadow-md p-2 rounded-sm text-sm flex justify-center items-center transition-all duration-300 gap-2 bg-background",
+        table.getFilteredSelectedRowModel().rows.length > 0
+          ? "-translate-y-5"
+          : "translate-y-[200%]"
+      )}
+    >
+      <DataTableActionBarSelection table={table} />
+      <Separator orientation="vertical" className="min-h-5" />
+      <div className="flex justify-center items-center gap-1">{children}</div>
+    </div>
+  );
+}
+
+interface DataTableActionBarSelectionProps<TData> {
+  table: Table<TData>;
+}
+
+export function DataTableActionBarSelection<TData>({
+  table,
+}: DataTableActionBarSelectionProps<TData>) {
+  return (
+    <div className="flex justify-center items-center border border-border py-1 px-2 rounded-sm gap-2 bg-background">
+      {table.getFilteredSelectedRowModel().rows.length} selected
+      <Separator orientation="vertical" className="min-h-4" />
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant={"ghost"}
+            size={"icon"}
+            className="size-5 rounded-xs"
+            onClick={() => table.resetRowSelection()}
+          >
+            <X className="size-3.5" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Clear Selection</p>
+        </TooltipContent>
+      </Tooltip>
+    </div>
+  );
+}
+
+interface DataTableActionBarActionProps extends ComponentProps<typeof Button> {
+  tooltip?: string;
+}
+
+export function DataTableActionBarAction({
+  tooltip,
+  size = "sm",
+  children,
+  ...props
+}: DataTableActionBarActionProps) {
+  const trigger = (
+    <Button
+      variant={"secondary"}
+      size={size}
+      {...props}
+      className={`${size === "icon" && "size-7"}`}
+    >
+      {children}
+    </Button>
+  );
+
+  if (!tooltip) return trigger;
+
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>{trigger}</TooltipTrigger>
+      <TooltipContent>
+        <p>{tooltip}</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+}
