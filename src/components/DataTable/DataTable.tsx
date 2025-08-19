53661cde112df47f28bfe90860b9601bb4a226db
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-19 19:46:33 +0500

    feat: create DataTable component

---
+++ src/components/DataTable/DataTable.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { flexRender, Table as TableType } from "@tanstack/react-table";
+import DataTablePagination from "./DataTablePagination";
+import { getCommonPinningStyles } from "@/lib/dataTableUtils";
+
+interface Props<TData> {
+  table: TableType<TData>;
+  pageSizes?: number[];
+}
+export default function DataTable<TData>({ table, pageSizes }: Props<TData>) {
+  return (
+    <div className="space-y-3">
+      <div className="border border-border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    style={getCommonPinningStyles(header.column)}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell
+                      key={cell.id}
+                      style={getCommonPinningStyles(cell.column)}
+                    >
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  className="h-24 text-center"
+                  colSpan={table.getAllColumns().length}
+                >
+                  No results found.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div>
+        <DataTablePagination table={table} pageSizes={pageSizes} />
+      </div>
+    </div>
+  );
+}
