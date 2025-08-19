3d8a769dd5c7d95b96774cfc15662f07160f3cdb
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-19 19:45:55 +0500

    feat: create utils for Data Table

---
+++ src/lib/dataTableUtils.ts
@@ -0,0 +1,45 @@
+import { Column, ColumnDef } from "@tanstack/react-table";
+import {
+  Parser,
+  parseAsArrayOf,
+  parseAsString,
+  parseAsTimestamp,
+} from "nuqs/server";
+import { CSSProperties } from "react";
+
+export function getCommonPinningStyles<TData>(
+  column: Column<TData>
+): CSSProperties {
+  const isPinned = column.getIsPinned();
+
+  return {
+    position: isPinned ? "sticky" : "relative",
+    right: isPinned === "right" ? `${column.getAfter()}px` : undefined,
+    left: isPinned === "left" ? `${column.getAfter()}px` : undefined,
+    zIndex: isPinned ? 1 : 0,
+    opacity: isPinned ? 0.97 : 1,
+    background: "var(--color-background)",
+    width: column.getSize(),
+  };
+}
+
+export function createDataTableFiltersParsers<TData, TValue = unknown>(
+  columns: ColumnDef<TData, TValue>[]
+) {
+  const filterableColumns = columns.filter(
+    (column) => column.enableColumnFilter
+  );
+
+  return filterableColumns.reduce<
+    Record<string, Parser<string> | Parser<string[]> | Parser<Date[]>>
+  >((acc, column) => {
+    if (column.meta?.options) {
+      acc[column.id ?? ""] = parseAsArrayOf(parseAsString);
+    } else if (column.meta?.variant === "DATE_RANGE") {
+      acc[column.id ?? ""] = parseAsArrayOf(parseAsTimestamp);
+    } else {
+      acc[column.id ?? ""] = parseAsString;
+    }
+    return acc;
+  }, {});
+}
