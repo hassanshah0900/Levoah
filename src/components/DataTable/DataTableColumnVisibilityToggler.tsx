044226e3fc0857240450e147514fa5328e32d5c9
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-19 19:36:45 +0500

    feat: create column visibility toggler.

---
+++ src/components/DataTable/DataTableColumnVisibilityToggler.tsx
@@ -0,0 +1,60 @@
+import { Table } from "@tanstack/react-table";
+import { Button } from "../ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "../ui/command";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { useMemo } from "react";
+
+interface Props<TData> {
+  table: Table<TData>;
+}
+export default function DataTableColumnVisibilityToggler<TData>({
+  table,
+}: Props<TData>) {
+  const columns = useMemo(
+    () => table.getAllColumns().filter((column) => column.getCanHide()),
+    [table]
+  );
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant={"outline"} size={"sm"}>
+          View <ChevronsUpDown className="opacity-60" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="p-0 w-40">
+        <Command>
+          <CommandInput placeholder={"Search columns..."} />
+          <CommandList>
+            <CommandEmpty>No columns found</CommandEmpty>
+            <CommandGroup>
+              {columns.map((column) => (
+                <CommandItem
+                  key={column.id}
+                  onSelect={() => {
+                    column.toggleVisibility(!column.getIsVisible());
+                  }}
+                  className="capitalize"
+                >
+                  <Check
+                    className={
+                      column.getIsVisible() ? "opacity-100" : "opacity-0"
+                    }
+                  />
+                  {column.id}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
