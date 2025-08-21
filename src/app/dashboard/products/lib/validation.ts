da262beb8f69fc4a720f7b62f072c0bb788027ee
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-22 01:51:18 +0500

    feat: create schema for product edit form and search param loader cache to parse products search params

--- src/app/dashboard/products/lib/validation.ts
+++ src/app/dashboard/products/lib/validation.ts
@@ -9,22 +9,44 @@ export const productFormSchema = z
   .object({
     image: z.instanceof(File, { error: "Image is required." }),
     title: z.string().min(1, "Title is required"),
-    price: z
-      .string()
-      .min(1, "Price is required.")
-      .transform((val) => parseInt(val)),
-    sale_price: z.string().transform((val) => (val ? parseInt(val) : 0)),
+    price: z.coerce.number().min(1, "Price is required."),
+    sale_price: z.coerce.number().optional().default(0),
     description: z.string(),
   })
   .refine((data) => data.price > data.sale_price, {
     error: "Sale price can't be greater than original price",
-    path: ["salePrice"],
+    path: ["sale_price"],
     when(payload) {
-      return z
-        .object({
-          price: z.number().min(1),
-          salePrice: z.number(),
-        })
+      return productFormSchema
+        .pick({ price: true, sale_price: true })
         .safeParse(payload.value).success;
     },
   });
+
+export type ProductFormSchemaType = z.infer<typeof productFormSchema>;
+
+export const productEditFormSchema = productFormSchema
+  .extend({
+    published: z.boolean().default(true),
+  })
+  .partial({ image: true })
+  .refine((data) => data.price > data.sale_price, {
+    error: "Sale price can't be greater than original price",
+    path: ["sale_price"],
+    when(payload) {
+      return productFormSchema
+        .pick({ price: true, sale_price: true })
+        .safeParse(payload.value).success;
+    },
+  });
+
+export type ProductEditFormSchemaType = z.infer<typeof productEditFormSchema>;
+
+export const productsPageSearchParamsCache = createSearchParamsCache({
+  pageIndex: parseAsInteger.withDefault(0),
+  pageSize: parseAsInteger.withDefault(10),
+  sorting: createSortingStateParser().withDefault([
+    { id: "created_at", desc: false },
+  ]),
+  title: parseAsString.withDefault(""),
+});
