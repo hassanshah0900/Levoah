72133cb6fa113d11aff632f7a8f799f03a3a2bde
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-23 14:25:31 +0500

    feat: add functionality of deletion and publish status updation

--- src/app/dashboard/products/components/ProductTableActionBar.tsx
+++ src/app/dashboard/products/components/ProductTableActionBar.tsx
@@ -1,9 +1,6 @@
 import DataTableActionBar, {
   DataTableActionBarAction,
 } from "@/components/DataTable/DataTableActionBar";
-import { table } from "console";
-import { CircleCheckBig, Trash } from "lucide-react";
-import ProductDeleteDialog from "./ProductDeleteDialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,12 +9,51 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Table } from "@tanstack/react-table";
+import { CircleCheckBig, Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import {
+  changeProductsPublishedStatus,
+  deleteMultipleProducts,
+} from "../lib/actions";
 import { Product } from "../lib/types";
+import ProductDeleteDialog from "./ProductDeleteDialog";
 
 interface Props {
   table: Table<Product>;
 }
 export default function ProductTableActionBar({ table }: Props) {
+  const router = useRouter();
+
+  function deleteProducts() {
+    const productIds = table
+      .getFilteredSelectedRowModel()
+      .rows.map((row) => row.original.id);
+    toast.promise(deleteMultipleProducts(productIds), {
+      loading: "Deleting products...",
+      success: () => {
+        router.refresh();
+        return "Products successfully deleted.";
+      },
+      error: ({ message }) => <div>{message}</div>,
+    });
+    console.log("Product Ids: ", productIds);
+  }
+
+  function changePublishedStatus(published: boolean) {
+    const productIds = table
+      .getFilteredSelectedRowModel()
+      .rows.map((row) => row.original.id);
+
+    toast.promise(changeProductsPublishedStatus(productIds, published), {
+      loading: "Changing status...",
+      success: () => {
+        router.refresh();
+        return "Successfully changed status.";
+      },
+      error: ({ message }) => <div>{message}</div>,
+    });
+  }
   return (
     <DataTableActionBar table={table}>
       <DropdownMenu>
@@ -28,8 +64,12 @@ export default function ProductTableActionBar({ table }: Props) {
         </DropdownMenuTrigger>
         <DropdownMenuContent align="start" sideOffset={10}>
           <DropdownMenuGroup>
-            <DropdownMenuItem>Publish</DropdownMenuItem>
-            <DropdownMenuItem>Save as draft</DropdownMenuItem>
+            <DropdownMenuItem onSelect={() => changePublishedStatus(true)}>
+              Publish
+            </DropdownMenuItem>
+            <DropdownMenuItem onSelect={() => changePublishedStatus(false)}>
+              Save as draft
+            </DropdownMenuItem>
           </DropdownMenuGroup>
         </DropdownMenuContent>
       </DropdownMenu>
@@ -39,7 +79,7 @@ export default function ProductTableActionBar({ table }: Props) {
             <Trash />
           </DataTableActionBarAction>
         }
-        onDelete={() => null}
+        onDelete={deleteProducts}
       />
     </DataTableActionBar>
   );
