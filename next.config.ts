f61c9fb485263f46ce2c5929ca14f879a55e3b03
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-23 14:42:22 +0500

    feat: extending next.js config to allow image from our supabase project.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
+import { NextURL } from "next/dist/server/web/next-url";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: new NextURL(process.env.NEXT_PUBLIC_SUPABASE_URL!).hostname,
+        port: "",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
   /* config options here */
 };
 
