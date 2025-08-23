5111d80f2b5bd7e7d00f4d452ee094a9963bdb71
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-23 14:48:56 +0500

    feat: create page for showing ProductsTable

---
+++ src/app/dashboard/products/page.tsx
@@ -0,0 +1,24 @@
+import { SearchParams } from "nuqs/server";
+import { getAllProducts } from "./lib/queries";
+import ProductsTable from "./components/ProductsTable";
+import { productsPageSearchParamsCache } from "./lib/validation";
+
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) {
+  const { pageIndex, pageSize, sorting, title } =
+    await productsPageSearchParamsCache.parse(searchParams);
+
+  const { products, count } = await getAllProducts({
+    filters: { pageIndex, pageSize, sorting, title },
+  });
+  console.log(products);
+
+  return (
+    <div>
+      <ProductsTable products={products} rowCount={count ?? 0} />
+    </div>
+  );
+}
