0dea1ffd75fd8060636bb72bf09bade43ad3d7bc
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-24 02:00:02 +0500

    feat: create useLocalStorage hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,28 @@
+export function useLocalStorage(key: string) {
+  function setItem(value: unknown) {
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  function getItem() {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : undefined;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  function removeItem() {
+    try {
+      localStorage.removeItem(key);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return { getItem, setItem, removeItem };
+}
