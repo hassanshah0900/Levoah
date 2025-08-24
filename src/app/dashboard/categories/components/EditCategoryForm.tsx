d675f9de424a9fa48bd7f91d8ba828c48238285d
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-24 16:02:09 +0500

    feat: create form for editing category

---
+++ src/app/dashboard/categories/components/EditCategoryForm.tsx
@@ -0,0 +1,71 @@
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Plus } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { Category } from "../lib/types";
+
+interface Props {
+  category: Category;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+export default function EditCategoryForm({
+  category,
+  open,
+  onOpenChange,
+}: Props) {
+  const form = useForm();
+  return (
+    <div>
+      <Drawer direction="right" open={open} onOpenChange={onOpenChange}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Edit Category</DrawerTitle>
+          </DrawerHeader>
+
+          <form action="" className="p-4 space-y-4">
+            <Form {...form}>
+              <FormField
+                name="Name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                name="slug"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Slug</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button className="w-full mt-4">Edit</Button>
+            </Form>
+          </form>
+        </DrawerContent>
+      </Drawer>
+    </div>
+  );
+}
