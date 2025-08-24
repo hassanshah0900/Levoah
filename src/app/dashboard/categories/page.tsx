4614ac385495d7dfff20b8236780794afc1ead1a
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-24 16:18:56 +0500

    feat: create a dashboard page for categories

---
+++ src/app/dashboard/categories/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CategoriesTable from "./components/CategoriesTable";
+
+export default function CategoriesPage() {
+  return (
+    <div>
+      <CategoriesTable />
+    </div>
+  );
+}
