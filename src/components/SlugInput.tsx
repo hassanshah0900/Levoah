dfeaa5a83eb719d92fc87db4db6547ae63fdf405
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-25 18:15:05 +0500

    feat: create input component for slug

---
+++ src/components/SlugInput.tsx
@@ -0,0 +1,31 @@
+import React, { ComponentPropsWithRef } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { useWatch } from "react-hook-form";
+import { slugify } from "@/lib/utils";
+
+interface Props extends Omit<ComponentPropsWithRef<typeof Input>, "onChange"> {
+  onChange: (text: string) => void;
+  slugSourceFieldName: string;
+}
+export default function SlugInput({
+  slugSourceFieldName,
+  onChange,
+  ...props
+}: Props) {
+  const value = useWatch({ name: slugSourceFieldName });
+  return (
+    <div className="flex justify-center items-center gap-2">
+      <Input {...props} onChange={(e) => onChange(e.currentTarget.value)} />
+      <Button
+        variant={"secondary"}
+        type="button"
+        onClick={() => {
+          onChange(slugify(value)!);
+        }}
+      >
+        Generate Slug
+      </Button>
+    </div>
+  );
+}
