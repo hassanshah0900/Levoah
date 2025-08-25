38a8108eccf37d38e6310a432f73a959aebc2cc3
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-25 18:18:38 +0500

    create: create checkout page

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CheckoutForm from "./components/CheckoutForm";
+
+export default function CheckoutPage() {
+  return (
+    <div>
+      <CheckoutForm />
+    </div>
+  );
+}
