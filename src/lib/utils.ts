c3b88b5701cf7efe9ff6a9af6e9af332d1dde947
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-25 18:00:43 +0500

    feat: create slugify to convert text to slug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function slugify(text: string) {
+  if (text)
+    return text
+      .trim()
+      .toLowerCase()
+      .replace(/[\s\W-]+/g, "-")
+      .replace(/^-+|-+$/g, "");
 }
