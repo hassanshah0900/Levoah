1c2ac4cd57723d640c99f775c87eae3c6292222a
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-26 00:04:17 +0500

    feat: create zod schema for categories

---
+++ src/app/dashboard/categories/lib/validation.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export const categorySchema = z.object({
+  name: z
+    .string()
+    .min(1, "Name is required.")
+    .max(100, "Name can be at max 100 chars"),
+  slug: z
+    .string()
+    .min(1, "Slug is required.")
+    .max(100, "Slug can be at max 100 characters"),
+  parent_category: z
+    .string()
+    .transform((field) => (field === "" ? undefined : field))
+    .optional(),
+});
+
+export type CategorySchemaType = z.infer<typeof categorySchema>;
