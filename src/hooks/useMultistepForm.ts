f60f8fb859ff60dab0a7d74b212fe84e926610e8
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-25 18:11:17 +0500

    feat: create useMultistepForm hook

---
+++ src/hooks/useMultistepForm.ts
@@ -0,0 +1,38 @@
+"use client";
+
+import { MultistepFormStep } from "@/app/checkout/lib/types";
+import { useState } from "react";
+
+export default function useMultistepForm(steps: MultistepFormStep[]) {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  function hasNextStep() {
+    return currentStep < steps.length;
+  }
+
+  function hasPreviousStep() {
+    return currentStep > 1;
+  }
+
+  function nextStep() {
+    if (hasNextStep()) setCurrentStep(currentStep + 1);
+  }
+
+  function previousStep() {
+    if (hasPreviousStep()) setCurrentStep(currentStep - 1);
+  }
+
+  function goTo(stepNumber: number) {
+    setCurrentStep(stepNumber);
+  }
+  return {
+    currentStep,
+    step: steps[currentStep - 1],
+    nextStep,
+    previousStep,
+    goTo,
+    hasNextStep,
+    hasPreviousStep,
+    steps,
+  };
+}
