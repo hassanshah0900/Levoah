380767e28cba57ff01691262f5b4c02cedbc5336
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-26 18:30:40 +0500

    feat: create table for product variants

---
+++ src/app/dashboard/products/components/ProductVariantsTable.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import DataTable from "@/components/DataTable/DataTable";
+import DataTableColumnVisibilityToggler from "@/components/DataTable/DataTableColumnVisibilityToggler";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { useDataTable } from "@/hooks/useDataTable";
+import { ProductVariant } from "../lib/types";
+import { productVariantColumns } from "./productVariantColumns";
+import ProductVariantForm from "./ProductVariantForm";
+
+interface Props {
+  product_id: number;
+  variants: ProductVariant[];
+}
+export default function ProductVariantsTable({ product_id, variants }: Props) {
+  const { table } = useDataTable({
+    data: variants,
+    columns: productVariantColumns,
+  });
+  return (
+    <div className="space-y-5 mt-5">
+      <div>
+        <SidebarTrigger />
+      </div>
+      <div className="flex justify-between items-center">
+        <DataTableColumnVisibilityToggler table={table} />
+        <ProductVariantForm product_id={product_id} />
+      </div>
+      <DataTable table={table}></DataTable>
+    </div>
+  );
+}
