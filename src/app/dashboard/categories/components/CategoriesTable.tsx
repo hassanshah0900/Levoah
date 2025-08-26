76bf560e9e7be508b638d63955e8e5c27544545a
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-26 14:07:10 +0500

    refactor: add categories as prop

--- src/app/dashboard/categories/components/CategoriesTable.tsx
+++ src/app/dashboard/categories/components/CategoriesTable.tsx
@@ -7,9 +7,14 @@ import { useDataTable } from "@/hooks/useDataTable";
 import { columns } from "./columns";
 import NewCategoryForm from "./NewCategoryForm";
 import CategoriesTableActionBar from "./CategoriesTableActionBar";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Category } from "../lib/types";
 
 const categories = [{ id: "134", name: "Men", slug: "men" }];
-export default function CategoriesTable() {
+interface Props {
+  categories: Category[];
+}
+export default function CategoriesTable({ categories }: Props) {
   const { table } = useDataTable({
     data: categories,
     columns,
@@ -22,6 +27,9 @@ export default function CategoriesTable() {
   });
   return (
     <div>
+      <div className="my-4">
+        <SidebarTrigger />
+      </div>
       <div className="flex justify-between items-center mb-5">
         <Input className="max-w-2xs" />
         <div className="flex justify-center items-center gap-2">
