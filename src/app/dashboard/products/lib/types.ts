ce91343ff4e75aa1ed46abf5ea32d0dbba7d314c
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-26 18:28:29 +0500

    feat: create ProductVariant type

--- src/app/dashboard/products/lib/types.ts
+++ src/app/dashboard/products/lib/types.ts
@@ -8,3 +8,12 @@ export interface Product {
   published: boolean;
   [key: string]: any;
 }
+
+export interface ProductVariant {
+  price: number;
+  quantity_in_stock: number;
+  frame_color: string;
+  lense_color: string;
+  product_id: number;
+  image_url: string;
+}
