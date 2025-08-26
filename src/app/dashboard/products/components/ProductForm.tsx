1a5a548096bc8330eb6f4fd9c0feb6921177d4c2
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-26 18:49:24 +0500

    refactor: update product form

--- src/app/dashboard/products/components/ProductForm.tsx
+++ src/app/dashboard/products/components/ProductForm.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import ImageInput from "@/components/ImageInput";
+import SlugInput from "@/components/SlugInput";
+import { ThemeToggler } from "@/components/ThemeToggler";
 import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
@@ -16,105 +19,105 @@ import {
   FormMessage,
 } from "../../../../components/ui/form";
 import { Input } from "../../../../components/ui/input";
+import CategoriesCombobox from "../../categories/components/CategoriesCombobox";
 import { createProduct } from "../lib/actions";
 import { productFormSchema, ProductFormSchemaType } from "../lib/validation";
 
 export default function ProductForm() {
   const [isPublished, setIsPublished] = useState(true);
+
   const form = useForm({
     defaultValues: {
-      image: undefined,
       title: "",
+      slug: "",
+      category: undefined,
       description: "",
-      price: "",
-      sale_price: "",
     },
     resolver: zodResolver(productFormSchema),
   });
 
-  async function onSubmit(product: ProductFormSchemaType) {
-    toast.promise(createProduct(product, isPublished), {
-      loading: "Creating Product...",
-      success: "Successfully created product",
-      error: (err) => err.message,
+  function onSubmit(data: ProductFormSchemaType) {
+    const product = { ...data, published: isPublished };
+    toast.promise(createProduct(product), {
+      loading: "Creating product...",
+      success: "Successfully created Product",
+      error: ({ message }) => message,
     });
-    form.reset();
-
-    setIsPublished(true);
   }
+
   return (
-    <form className="space-y-5" onSubmit={form.handleSubmit(onSubmit)}>
-      <Form {...form}>
-        <FormField
-          name="image"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <ImageInput onChange={field.onChange} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Title</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          name="price"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Price</FormLabel>
-              <FormControl>
-                <Input {...field} type="number" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          name="sale_price"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Sale Price</FormLabel>
-              <FormControl>
-                <Input {...field} type="number" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <FormControl>
-                <Textarea {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex gap-4">
-          <Button type="submit">Publish</Button>
-          <Button
-            type="submit"
-            variant={"secondary"}
-            onClick={() => setIsPublished(false)}
-          >
-            Save as Draft
-          </Button>
+    <form className="mb-10" onSubmit={form.handleSubmit(onSubmit)}>
+      <div className="sticky top-0 mb-10 z-10">
+        <div className="flex justify-between items-center bg-background p-1.5">
+          <div className="space-x-4">
+            <SidebarTrigger />
+            <ThemeToggler />
+          </div>
+          <div className="space-x-4">
+            <Button
+              type="submit"
+              variant={"secondary"}
+              onClick={() => setIsPublished(false)}
+            >
+              Save as Draft
+            </Button>
+            <Button type="submit">Publish</Button>
+          </div>
         </div>
-      </Form>
+        <Separator />
+      </div>
+      <div className="space-y-4">
+        <Form {...form}>
+          <FormField
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="slug"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Slug</FormLabel>
+                <FormControl>
+                  <SlugInput {...field} slugSourceFieldName={"title"} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="category"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Category</FormLabel>
+                <FormControl>
+                  <CategoriesCombobox {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </Form>
+      </div>
     </form>
   );
 }
