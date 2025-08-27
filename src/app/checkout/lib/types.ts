a8344af80178ba4c7cfc8991083495a07c3934db
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-27 19:35:49 +0500

    refactor: move complete checkout route to app group route

--- src/app/checkout/lib/types.ts
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from "react";
-import { AddressSchemaType } from "./validation";
-
-export interface MultistepFormStep {
-  label: string;
-  element: ReactElement;
-  fields: (keyof AddressSchemaType)[];
-}
