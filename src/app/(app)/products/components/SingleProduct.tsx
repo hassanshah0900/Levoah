27b68abcacf30b27ed980761687c75dd0fd08e4a
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-28 19:08:16 +0500

    feat: make SingleProduct component responsive

--- src/app/(app)/products/components/SingleProduct.tsx
+++ src/app/(app)/products/components/SingleProduct.tsx
@@ -21,16 +21,20 @@ export default function SingleProduct({ slug }: { slug: string }) {
   const [currentVariant, setCurrentVariant] = useState(product.variants[0]);
 
   return (
-    <div className="grid grid-cols-2 gap-10 py-10">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-10 py-10">
       <ProductImage
         src={currentVariant.image_url}
         alt=""
-        className="shadow-highlight shadow-[0_0_10px]"
+        className="shadow-highlight shadow-[0_0_5px] md:shadow-[0_0_10px] rounded-sm"
       />
-      <div className="space-y-5">
-        <div className="">
-          <h1 className="text-3xl font-semibold uppercase">{product.title}</h1>
-          <p className="text-2xl">Rs {currentVariant.price}</p>
+      <div className="space-y-4 md:space-y-5">
+        <div>
+          <h1 className="text-xl md:text-3xl font-semibold uppercase leading-tight md:leading-normal">
+            {product.title}
+          </h1>
+          <p className="text-xl md:text-2xl leading-tight md:leading-normal">
+            Rs {currentVariant.price}
+          </p>
         </div>
         <Separator />
         <Variants
@@ -39,23 +43,28 @@ export default function SingleProduct({ slug }: { slug: string }) {
           onVariantSelect={(variant) => setCurrentVariant(variant)}
         />
 
-        <div className="mt-5">
-          <p className="font-semibold text-lg ">
+        <div>
+          <p className="font-semibold md:text-lg">
             Frame{"   "}
-            <span className="text-base text-muted-foreground font-normal normal-case ms-2">
+            <span className="text-sm md:text-base text-muted-foreground font-normal normal-case ms-1 md:ms-2">
               {currentVariant.frame_color}
             </span>
           </p>
-          <p className="font-semibold text-lg">
+          <p className="font-semibold md:text-lg">
             Lense{"   "}
-            <span className="text-base text-muted-foreground font-normal normal-case ms-2">
+            <span className="text-sm md:text-base text-muted-foreground font-normal normal-case ms-1 md:ms-2">
               {currentVariant.lense_color}
             </span>
           </p>
         </div>
-        <p>{product.description}</p>
+        <p className="text-sm md:text-base">
+          {product.description} Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Harum molestiae quod cumque nobis dolor maxime, sit
+          magni quas, placeat architecto nulla autem ipsam, assumenda nesciunt
+          perspiciatis modi labore a rerum!
+        </p>
         <Separator />
-        <Button className="w-full mt-5">Add to Cart</Button>
+        <Button className="w-full">Add to Cart</Button>
       </div>
     </div>
   );
@@ -71,16 +80,18 @@ function Variants({
   currentVariant: ProductVariant;
 }) {
   return (
-    <div className="space-y-2">
-      <p className="font-semibold text-sm">{variants.length} Colors</p>
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(4rem,1fr))] gap-3">
+    <div className="space-y-1 md:space-y-2">
+      <p className="font-semibold text-xs md:text-sm text-muted-foreground">
+        {variants.length} Colors
+      </p>
+      <div className="grid grid-cols-5 md:grid-cols-[repeat(auto-fill,minmax(4rem,1fr))] gap-1 md:gap-3">
         {variants.map((variant) => (
           <button
             key={variant.id}
             className={cn(
               "w-full rounded-xs overflow-hidden",
               variant.id === currentVariant.id &&
-                "shadow-[0_0_10px] shadow-highlight"
+                "shadow-[0_0_4px] md:shadow-[0_0_10px] shadow-highlight"
             )}
             onClick={() => onVariantSelect(variant)}
           >
