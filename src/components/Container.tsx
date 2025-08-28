c18a0145430c06f964d1c0193ba2447a72bf960b
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-28 19:12:12 +0500

    feat: make Container responsive

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,5 +1,5 @@
 import React, { PropsWithChildren } from "react";
 
 export default function Container({ children }: PropsWithChildren) {
-  return <div className="max-w-[1100px] mx-auto px-5">{children}</div>;
+  return <div className="max-w-[1100px] mx-auto px-2 md:px-4">{children}</div>;
 }
