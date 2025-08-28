acf3095a5aaf13c99eabde3fc749aa03c3d55560
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-28 13:53:03 +0500

    refactor: change getAllCategories structure to suit react query

--- src/app/dashboard/categories/lib/queries.ts
+++ src/app/dashboard/categories/lib/queries.ts
@@ -1,12 +1,16 @@
 "use server";
 
 import { createClient } from "@/supabase/server";
+import { Category } from "./types";
 
 export async function getAllCategories() {
   const supabase = await createClient();
 
-  const { data, error } = await supabase.from("categories").select("*");
+  const { data, error, count } = await supabase
+    .from("categories")
+    .select("*", { count: "exact" });
+
   if (error) throw error;
 
-  return { data };
+  return { categories: data as Category[], count };
 }
