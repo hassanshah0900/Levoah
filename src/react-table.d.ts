014f0551fae9878d7f79849382da58748236522a
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-31 19:32:23 +0500

    feat: extend meta property of columns in react table

--- src/react-table.d.ts
+++ src/react-table.d.ts
@@ -1,8 +1,14 @@
 import "@tanstack/react-table"; //or vue, svelte, solid, qwik, etc.
+import { Component } from "react";
 
 declare module "@tanstack/react-table" {
   interface ColumnMeta<TData extends RowData, TValue> {
-    options: [];
+    label?: string;
+    options: {
+      value: string;
+      label: string;
+      icon?: FC<SVGProps<SVGSVGElement>>;
+    }[];
     variant: "MULTI_SELECT" | "DATE_RANGE";
   }
 }
