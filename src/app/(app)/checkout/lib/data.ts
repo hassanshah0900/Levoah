f1de200a3eb60a7586dccf768e5ed1f9fb6ca088
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-31 19:13:45 +0500

    feat: create provinces data for checkout page

---
+++ src/app/(app)/checkout/lib/data.ts
@@ -0,0 +1,9 @@
+export const provinces = [
+  "Punjab",
+  "Sindh",
+  "Khyber Pakhtunkhwa (KPK)",
+  "Balochistan",
+  "Islamabad Capital Territory (ICT)",
+  "Gilgit-Baltistan (GB)",
+  "Azad Jammu & Kashmir (AJK)",
+];
