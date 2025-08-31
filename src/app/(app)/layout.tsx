3306f932f9a359f888b8dba445a990d5e28043f3
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-31 19:43:20 +0500

    fix: wrap (app) route with ShoppingCartProvider

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,18 +1,21 @@
 import FloatingShoppingCart from "@/components/FloatingShoppingCart";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { ShoppingCartProvider } from "@/contexts/ShoppingCartContext";
 import React, { PropsWithChildren } from "react";
 
 export default function layout({ children }: PropsWithChildren) {
   return (
-    <div>
-      <div className="flex justify-center items-center bg-secondary text-secondary-foreground font-semibold text-sm p-0.5">
-        Order Over 5000 to get free delivery
+    <ShoppingCartProvider>
+      <div>
+        <div className="flex justify-center items-center bg-secondary text-secondary-foreground font-semibold text-sm p-0.5">
+          Order Over 5000 to get free delivery
+        </div>
+        <Navbar />
+        {children}
+        <FloatingShoppingCart />
+        <Footer />
       </div>
-      <Navbar />
-      {children}
-      <FloatingShoppingCart />
-      <Footer />
-    </div>
+    </ShoppingCartProvider>
   );
 }
