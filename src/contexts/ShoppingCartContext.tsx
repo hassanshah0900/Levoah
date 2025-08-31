167ba8de290b13c84c0802aa6ac4d0fbbfac2e8e
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-31 15:38:21 +0500

    feat: create ShoppingCartContext

---
+++ src/contexts/ShoppingCartContext.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import {
+  createContext,
+  PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+export interface ShoppingCartItem {
+  id?: string;
+  product_id: number;
+  variant_id: number;
+  image_url: string;
+  title: string;
+  price: number;
+  quantity: number;
+  frame_color: string;
+  lense_color: string;
+}
+
+interface ShoppingCartContextType {
+  cartItems: ShoppingCartItem[];
+  addCartItem: (item: ShoppingCartItem) => void;
+  deleteCartItem: (id: ShoppingCartItem["id"]) => void;
+  incrementQuantity: (id: ShoppingCartItem["id"]) => void;
+  decrementQuantity: (id: ShoppingCartItem["id"]) => void;
+  isInCart: (
+    productId: ShoppingCartItem["product_id"],
+    variantId: ShoppingCartItem["variant_id"]
+  ) => boolean;
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+}
+
+const ShoppingCartContext = createContext<ShoppingCartContextType | null>(null);
+
+export function ShoppingCartProvider({ children }: PropsWithChildren) {
+  const { getItem, setItem } = useLocalStorage("shopping cart");
+  const [cartItems, setCartItems] = useState<ShoppingCartItem[]>(
+    () => getItem() || []
+  );
+  const [isOpen, setIsOpen] = useState(false);
+
+  function addCartItem(item: ShoppingCartItem) {
+    setCartItems((cartItems) => {
+      const existingCartItem = cartItems.find(
+        (cartItem) =>
+          cartItem.product_id == item.product_id &&
+          cartItem.variant_id == item.variant_id
+      );
+
+      if (existingCartItem) {
+        return cartItems.map((item) =>
+          item.product_id === existingCartItem.product_id &&
+          item.variant_id === existingCartItem.variant_id
+            ? { ...existingCartItem, quantity: existingCartItem.quantity + 1 }
+            : item
+        );
+      }
+      return [{ ...item, id: crypto.randomUUID() }, ...cartItems];
+    });
+  }
+
+  function deleteCartItem(id: ShoppingCartItem["id"]) {
+    setCartItems(cartItems.filter((item) => item.id !== id));
+  }
+
+  function incrementQuantity(id: ShoppingCartItem["id"]) {
+    const cartItem = cartItems.find((item) => item.id === id);
+    if (cartItem)
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : item
+        )
+      );
+  }
+
+  function decrementQuantity(id: ShoppingCartItem["id"]) {
+    setCartItems((cartItems) => {
+      const cartItem = cartItems.find((item) => item.id === id);
+      if (cartItem) {
+        if (cartItem.quantity <= 1)
+          return cartItems.filter((item) => item.id !== id);
+        return cartItems.map((item) =>
+          item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+        );
+      }
+      return cartItems;
+    });
+  }
+
+  function isInCart(
+    productId: ShoppingCartItem["product_id"],
+    variantId: ShoppingCartItem["variant_id"]
+  ) {
+    return !!cartItems.find(
+      (item) => item.product_id === productId && item.variant_id === variantId
+    );
+  }
+
+  useEffect(() => {
+    setItem(cartItems);
+  }, [cartItems]);
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        cartItems,
+        addCartItem,
+        deleteCartItem,
+        incrementQuantity,
+        decrementQuantity,
+        isInCart,
+        isOpen,
+        setIsOpen,
+      }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+}
+
+export function useShoppingCart() {
+  const context = useContext(ShoppingCartContext);
+  if (!context)
+    throw new Error(
+      "useShoppingCart hook can only be used inside its Provider."
+    );
+  return context;
+}
