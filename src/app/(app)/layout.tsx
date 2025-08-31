97ead3ac5ee9b9d2d5503fe51d06c916752b305f
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-08-31 19:38:55 +0500

    feat: extend app layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,5 +1,18 @@
+import FloatingShoppingCart from "@/components/FloatingShoppingCart";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 import React, { PropsWithChildren } from "react";
 
 export default function layout({ children }: PropsWithChildren) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <div className="flex justify-center items-center bg-secondary text-secondary-foreground font-semibold text-sm p-0.5">
+        Order Over 5000 to get free delivery
+      </div>
+      <Navbar />
+      {children}
+      <FloatingShoppingCart />
+      <Footer />
+    </div>
+  );
 }
