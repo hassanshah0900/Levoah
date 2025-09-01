7ddd2e017026b9b588d9633f950b81c392589784
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-02 00:43:51 +0500

    feat: make cart icon functional

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import { Search, ShoppingBag } from "lucide-react";
 import Link from "next/link";
 import Container from "./Container";
+import { useShoppingCart } from "@/contexts/ShoppingCartContext";
 
 export default function Navbar() {
+  const { cartItems, setIsOpen } = useShoppingCart();
+
   return (
-    <div className="bg-primary text-primary-foreground sticky top-0 z-10 border-y-2 border-highlight/60">
+    <div className="bg-primary text-primary-foreground sticky top-0 z-10 border-y-0 border-highlight/60 ">
       <Container>
         <div className="flex justify-between items-center py-2">
           <div className="w-10 h-10 bg-background"></div>
@@ -39,10 +44,15 @@ export default function Navbar() {
             <button className="text-secondary [&_svg]:size-5 cursor-pointer">
               <Search />
             </button>
-            <button className="relative text-secondary [&_svg]:size-5 cursor-pointer">
-              <div className="h-5 w-5 rounded-full bg-highlight text-xs absolute -translate-2 flex justify-center items-center">
-                5
-              </div>
+            <button
+              className="relative text-secondary [&_svg]:size-5 cursor-pointer"
+              onClick={() => setIsOpen(true)}
+            >
+              {cartItems.length ? (
+                <div className="h-5 w-5 rounded-full bg-highlight text-xs absolute -translate-2 flex justify-center items-center">
+                  {cartItems.length}
+                </div>
+              ) : null}
               <ShoppingBag />
             </button>
           </div>
