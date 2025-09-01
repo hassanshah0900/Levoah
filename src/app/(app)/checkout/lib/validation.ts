5cb15bbfddb0b5d1ade54ca6d2dba1d7b0c94478
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-02 00:24:09 +0500

    refactor: change the structure of checkout schema

--- src/app/(app)/checkout/lib/validation.ts
+++ src/app/(app)/checkout/lib/validation.ts
@@ -1,14 +1,19 @@
 import z, { email } from "zod";
 
 const REQUIRED_MESSAGE = "This field is required.";
-export const addressSchema = z.object({
-  full_name: z.string().min(1, { error: REQUIRED_MESSAGE }),
-  country: z.string().min(1, { error: REQUIRED_MESSAGE }),
-  city: z.string().min(1, { error: REQUIRED_MESSAGE }),
-  province: z.string().min(1, { error: REQUIRED_MESSAGE }),
-  address: z.string().min(1, { error: REQUIRED_MESSAGE }),
-  email: z.email().min(1, { error: REQUIRED_MESSAGE }),
-  phone: z.string().min(1, { error: REQUIRED_MESSAGE }),
+
+export const checkoutFormSchema = z.object({
+  shipping_address: z.object({
+    full_name: z.string().min(1, { error: REQUIRED_MESSAGE }),
+    country: z.string().min(1, { error: REQUIRED_MESSAGE }),
+    city: z.string().min(1, { error: REQUIRED_MESSAGE }),
+    province: z.string().min(1, { error: REQUIRED_MESSAGE }),
+    address: z.string().min(1, { error: REQUIRED_MESSAGE }),
+    email: z.email().min(1, { error: REQUIRED_MESSAGE }),
+    phone: z.string().min(1, { error: REQUIRED_MESSAGE }),
+    postal_code: z.string().min(1, { error: REQUIRED_MESSAGE }),
+  }),
+  payment_method: z.string(),
 });
 
-export type AddressSchemaType = z.infer<typeof addressSchema>;
+export type CheckoutFormSchemaType = z.infer<typeof checkoutFormSchema>;
