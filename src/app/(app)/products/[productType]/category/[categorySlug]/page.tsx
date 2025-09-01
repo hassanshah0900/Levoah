b1e829e40d00b01ad4b6ac964f60d3124b0059fb
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-02 00:23:18 +0500

    feat: create page for display products of a specific category

---
+++ src/app/(app)/products/[productType]/category/[categorySlug]/page.tsx
@@ -0,0 +1,44 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import React from "react";
+import {
+  getCategoryBySlug,
+  getProductsWithVariants,
+} from "../../../lib/queries";
+import ProductsGrid from "../../../components/ProductsGrid";
+import { PAGE_SIZE } from "../../../lib/data";
+
+export default async function CategoryPage({
+  params,
+}: {
+  params: Promise<{ categorySlug: string; productType: string }>;
+}) {
+  const { categorySlug, productType } = await params;
+
+  const queryClient = new QueryClient();
+
+  await Promise.all([
+    queryClient.prefetchInfiniteQuery({
+      queryKey: ["products with variants", categorySlug, productType],
+      queryFn: ({ pageParam }) =>
+        getProductsWithVariants({ pageIndex: pageParam, pageSize: PAGE_SIZE }),
+      initialPageParam: 0,
+    }),
+
+    queryClient.prefetchQuery({
+      queryKey: ["categories", categorySlug],
+      queryFn: () => getCategoryBySlug(categorySlug),
+    }),
+  ]);
+
+  const state = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={state}>
+      <ProductsGrid categorySlug={categorySlug} productType={productType} />
+    </HydrationBoundary>
+  );
+}
