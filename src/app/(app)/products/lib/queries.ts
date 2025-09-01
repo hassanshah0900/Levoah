2a01a9cf4c8aaeffb66c195491e64bda56f6d751
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-02 00:17:57 +0500

    feat: add server functions for product display

--- src/app/(app)/products/lib/queries.ts
+++ src/app/(app)/products/lib/queries.ts
@@ -1,7 +1,34 @@
 "use server";
 
+import { Category } from "@/app/dashboard/categories/lib/types";
 import { createClient } from "@/supabase/server";
 import { ProductWithVariants } from "@/types/products.types";
+
+interface GetProductsWithVariantsProps {
+  pageIndex: number;
+  pageSize: number;
+}
+export async function getProductsWithVariants({
+  pageIndex,
+  pageSize,
+}: GetProductsWithVariantsProps) {
+  const supabase = await createClient();
+
+  const rangeStart = pageIndex * pageSize;
+  const rangeEnd = rangeStart + pageSize - 1;
+  const { data, count, error } = await supabase
+    .from("products_with_variants")
+    .select("*", { count: "exact" })
+    .range(rangeStart, rangeEnd);
+
+  if (error) throw error;
+
+  return { products: data as ProductWithVariants[], count };
+}
+
+export async function getProductsByCategory() {
+  const supabase = await createClient();
+}
 
 export async function getProductWithVariants(slug: string) {
   const supabase = await createClient();
@@ -15,4 +42,19 @@ export async function getProductWithVariants(slug: string) {
   if (error) throw error;
 
   return { product: data as ProductWithVariants };
+}
+
+export async function getCategoryBySlug(slug: string) {
+  if (!slug) return null;
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .from("categories")
+    .select("*")
+    .eq("slug", slug)
+    .single();
+
+  if (error) throw error;
+
+  return data as Category;
 }
