9894e5f1ee4c1f2cd8623741c2a0520134bcfadc
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-02 01:08:32 +0500

    refactor: remove ThemeProvider

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,19 +1,16 @@
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { ThemeProvider } from "@/providers/ThemeProvider";
 import { PropsWithChildren } from "react";
 import DashboardSidebar from "./components/DashboardSidebar";
 
 export default function DashboardLayout({ children }: PropsWithChildren) {
   return (
-    <ThemeProvider attribute={"class"} defaultTheme="system">
-      <SidebarProvider>
-        <DashboardSidebar />
-        <SidebarInset>
-          <div className="w-full">
-            <div className="px-4">{children}</div>
-          </div>
-        </SidebarInset>
-      </SidebarProvider>
-    </ThemeProvider>
+    <SidebarProvider>
+      <DashboardSidebar />
+      <SidebarInset>
+        <div className="w-full">
+          <div className="px-4">{children}</div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
