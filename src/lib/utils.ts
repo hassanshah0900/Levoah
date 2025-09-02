8a13b0d5185255195ebc47fbfc99f6ec15d06526
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-02 16:18:18 +0500

    feat: create utility to compress images

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import imageCompression from "browser-image-compression";
+import { toast } from "sonner";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -23,4 +25,17 @@ export function getNextPageIndex(
   const lastPageIndex = numberofPages - 1;
 
   if (currentPageIndex < lastPageIndex) return currentPageIndex + 1;
+}
+
+export async function compressImage(file: File) {
+  if (!file) return;
+  try {
+    return await imageCompression(file, {
+      maxWidthOrHeight: 2000,
+      fileType: "image/webp",
+      initialQuality: 0.8,
+    });
+  } catch (error) {
+    toast.error("Coudn't compress image.");
+  }
 }
