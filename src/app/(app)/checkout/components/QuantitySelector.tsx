6f9298c8490201ccac0c8137732a251d1899a9c5
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-03 20:15:36 +0500

    feat: create QuantitySelector component

---
+++ src/app/(app)/checkout/components/QuantitySelector.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import {
+  ShoppingCartItem,
+  useShoppingCart,
+} from "@/contexts/ShoppingCartContext";
+import { cn } from "@/lib/utils";
+import { Minus, Plus } from "lucide-react";
+import React from "react";
+
+interface Props {
+  cartItem: ShoppingCartItem;
+  className?: string;
+  orientation?: "HORIZONTAL" | "VERTICAL";
+}
+export default function QuantitySelector({
+  cartItem,
+  className,
+  orientation = "HORIZONTAL",
+}: Props) {
+  const { incrementQuantity, decrementQuantity } = useShoppingCart();
+  return (
+    <div
+      className={cn(
+        "flex items-center justify-center gap-1.5",
+        orientation === "VERTICAL" ? "flex-col" : "flex-row-reverse",
+        className
+      )}
+    >
+      <Button
+        variant={"outline"}
+        size={"icon"}
+        className="size-6"
+        onClick={() => incrementQuantity(cartItem.id)}
+      >
+        <Plus className="size-3" />
+      </Button>
+      <div className="text-sm">{cartItem.quantity}</div>
+      <Button
+        variant={"outline"}
+        size={"icon"}
+        className="size-6"
+        onClick={() => decrementQuantity(cartItem.id)}
+      >
+        <Minus className="size-3" />
+      </Button>
+    </div>
+  );
+}
