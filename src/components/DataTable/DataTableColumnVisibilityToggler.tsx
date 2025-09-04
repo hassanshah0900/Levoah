9f2e32eacf53e4c822d4c2e87b43c436d874d1af
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-04 23:27:06 +0500

    refactor: expose className and align props

--- src/components/DataTable/DataTableColumnVisibilityToggler.tsx
+++ src/components/DataTable/DataTableColumnVisibilityToggler.tsx
@@ -10,51 +10,58 @@ import {
   CommandList,
 } from "../ui/command";
 import { Check, ChevronsUpDown } from "lucide-react";
-import { useMemo } from "react";
+import { ComponentProps, useMemo } from "react";
+import { cn } from "@/lib/utils";
 
 interface Props<TData> {
   table: Table<TData>;
+  className?: string;
+  align?: ComponentProps<typeof PopoverContent>["align"];
 }
 export default function DataTableColumnVisibilityToggler<TData>({
   table,
+  className,
+  align,
 }: Props<TData>) {
   const columns = useMemo(
     () => table.getAllColumns().filter((column) => column.getCanHide()),
     [table]
   );
   return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button variant={"outline"} size={"sm"}>
-          View <ChevronsUpDown />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="p-0 w-40">
-        <Command>
-          <CommandInput placeholder={"Search columns..."} />
-          <CommandList>
-            <CommandEmpty>No columns found</CommandEmpty>
-            <CommandGroup>
-              {columns.map((column) => (
-                <CommandItem
-                  key={column.id}
-                  onSelect={() => {
-                    column.toggleVisibility(!column.getIsVisible());
-                  }}
-                  className="capitalize"
-                >
-                  <Check
-                    className={
-                      column.getIsVisible() ? "opacity-100" : "opacity-0"
-                    }
-                  />
-                  {column.id}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+    <div className={cn(className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button variant={"outline"} size={"sm"}>
+            View <ChevronsUpDown />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="p-0 w-40" align={align}>
+          <Command>
+            <CommandInput placeholder={"Search columns..."} />
+            <CommandList>
+              <CommandEmpty>No columns found</CommandEmpty>
+              <CommandGroup>
+                {columns.map((column) => (
+                  <CommandItem
+                    key={column.id}
+                    onSelect={() => {
+                      column.toggleVisibility(!column.getIsVisible());
+                    }}
+                    className="capitalize"
+                  >
+                    <Check
+                      className={
+                        column.getIsVisible() ? "opacity-100" : "opacity-0"
+                      }
+                    />
+                    {column.id}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 }
