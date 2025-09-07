10bb5c70d7b67ca82da3541ec29b5e55de7fea6a
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-07 16:25:57 +0500

    refactor: remove prefetching logic in the page file

--- src/app/(app)/products/[productType]/[slug]/page.tsx
+++ src/app/(app)/products/[productType]/[slug]/page.tsx
@@ -1,35 +1,16 @@
 export const runtime = "edge";
 
 import Container from "@/components/Container";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
 import SingleProduct from "../../components/SingleProduct";
-import { getProductWithVariants } from "../../lib/queries";
+import { Suspense } from "react";
+import SingleProductPageSkeleton from "../../components/SingleProductSkeleton";
 
-export default async function ProductPage({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) {
-  const { slug } = await params;
-
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ["single product with variants", { slug }],
-    queryFn: () => getProductWithVariants(slug),
-  });
-
-  const state = dehydrate(queryClient);
-
+export default async function ProductPage() {
   return (
-    <HydrationBoundary state={state}>
-      <Container>
-        <SingleProduct slug={slug} />
-      </Container>
-    </HydrationBoundary>
+    <Container>
+      <Suspense fallback={<SingleProductPageSkeleton />}>
+        <SingleProduct />
+      </Suspense>
+    </Container>
   );
 }
