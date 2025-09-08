72db1b2fefdb5a0b87d75e724d67d27662dc8780
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-08 13:46:16 +0500

    feat: create skeleton for related products on product page

---
+++ src/app/(app)/products/components/RelatedProductsSkeleton.tsx
@@ -0,0 +1,18 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+import ProductCardSkeleton from "./ProductCardSkeleton";
+
+export default function RelatedProductsSectionSkeleton() {
+  return (
+    <div className="mt-20 space-y-10">
+      <div className="flex justify-center items-center">
+        <Skeleton className="h-10 w-1/4" />
+      </div>
+      <div className="grid grid-cols-4 gap-5">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <ProductCardSkeleton key={idx} />
+        ))}
+      </div>
+    </div>
+  );
+}
