1b9260aac3fb145dfa33476c492ac11ff1904a93
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-11 02:36:37 +0500

    feat: create dashboard brands page

---
+++ src/app/dashboard/(main)/brands/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import BrandsTable from "./components/BrandsTable";
+
+export default function BrandPage() {
+  return (
+    <div>
+      <BrandsTable />
+    </div>
+  );
+}
