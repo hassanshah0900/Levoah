06854bc4a0b7476c8ed8c2e4809de33f6d50d08a
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-11 18:27:18 +0500

    feat: create select component for selecting glasses type

---
+++ src/app/dashboard/(main)/glasses/components/GlassesTypeSelect.tsx
@@ -0,0 +1,23 @@
+import { getCategoriesByProductType } from "@/app/(app)/[...categories]/lib/queries";
+import FormSelect from "@/components/FormSelect";
+import { useQuery } from "@tanstack/react-query";
+import { ControllerFieldState, ControllerRenderProps } from "react-hook-form";
+
+export default function GlassesTypeSelect({
+  ...props
+}: ControllerRenderProps & Pick<ControllerFieldState, "invalid">) {
+  const { data: categories } = useQuery({
+    queryKey: ["categories", "glasses"],
+    queryFn: () => getCategoriesByProductType("glasses", true),
+  });
+
+  const items = categories
+    ? categories.map(({ name, id }) => ({
+        label: name,
+        value: String(id),
+      }))
+    : [{ label: "Loading...", value: "loading" }];
+  return (
+    <FormSelect items={items} {...props} placeholder="Select Glasses Type" />
+  );
+}
