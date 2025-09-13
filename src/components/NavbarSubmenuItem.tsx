c0ec7c7327274b1a5f56fd7e353dafccf74aa187
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-13 22:56:38 +0500

    feat: create navbar submenu item

---
+++ src/components/NavbarSubmenuItem.tsx
@@ -0,0 +1,18 @@
+import ProductImage from "@/app/(app)/[...categories]/components/ProductImage";
+
+interface Props {
+  label: string;
+  imgUrl?: string;
+}
+export default function NavbarCategory({ label, imgUrl }: Props) {
+  return (
+    <div className="flex justify-start items-center font-semibold gap-4">
+      <ProductImage
+        src={imgUrl}
+        alt=""
+        className="size-16 rounded-full shadow-md"
+      />
+      <p>{label}</p>
+    </div>
+  );
+}
