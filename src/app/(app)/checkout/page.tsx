602fd30f7ea100cedc1aa6c6e5ee52d85c79c7b7
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-18 16:19:06 +0500

    refactor: replace multi step checkout with single page checkout

--- src/app/(app)/checkout/page.tsx
+++ src/app/(app)/checkout/page.tsx
@@ -1,13 +1,12 @@
-import React from "react";
-import CheckoutForm from "./components/CheckoutForm";
 import Container from "@/components/Container";
+import SinglePageCheckout from "./components/SinglePageCheckout";
 
 export default function CheckoutPage() {
   return (
     <div className="py-10">
       <Container>
         <h1 className="text-2xl font-semibold mb-5">Checkout</h1>
-        <CheckoutForm />
+        <SinglePageCheckout />
       </Container>
     </div>
   );
