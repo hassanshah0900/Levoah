63c80c8b2b696f408e20e9f19908d7d793f39789
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-18 16:23:30 +0500

    feat: add checkout page related types

--- src/app/(app)/checkout/lib/types.ts
+++ src/app/(app)/checkout/lib/types.ts
@@ -9,3 +9,16 @@ export interface MultistepFormStep {
     | `shipping_address.${keyof CheckoutFormSchemaType["shipping_address"]}`
   )[];
 }
+
+export type PaymentMethod = "CASHONDELIVERY" | "PAYFAST";
+
+export type OrderStatus =
+  | "Pending"
+  | "Processing"
+  | "Shipped"
+  | "Delivered"
+  | "Cancelled"
+  | "Returned"
+  | "Failed Delivery";
+
+export type OrderPaymentStatus = "paid" | "unpaid" | "cancelled";
