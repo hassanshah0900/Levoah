f7cba212cfaa7deb67eb4b52624bb609c3b7c23f
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-19 19:03:43 +0500

    chore: pull latest schema from database

---
+++ src/db/drizzle/relations.ts
@@ -0,0 +1,80 @@
+import { relations } from "drizzle-orm/relations";
+import { products, productVariants, categories, images, orders, orderItems, productCategories, permissions, rolePermissions, roles } from "./schema";
+
+export const productVariantsRelations = relations(productVariants, ({one, many}) => ({
+	product: one(products, {
+		fields: [productVariants.productId],
+		references: [products.id]
+	}),
+	images: many(images),
+}));
+
+export const productsRelations = relations(products, ({many}) => ({
+	productVariants: many(productVariants),
+	images: many(images),
+	productCategories: many(productCategories),
+}));
+
+export const categoriesRelations = relations(categories, ({one, many}) => ({
+	category: one(categories, {
+		fields: [categories.parentCategory],
+		references: [categories.id],
+		relationName: "categories_parentCategory_categories_id"
+	}),
+	categories: many(categories, {
+		relationName: "categories_parentCategory_categories_id"
+	}),
+	productCategories: many(productCategories),
+}));
+
+export const imagesRelations = relations(images, ({one}) => ({
+	product: one(products, {
+		fields: [images.productId],
+		references: [products.id]
+	}),
+	productVariant: one(productVariants, {
+		fields: [images.variantId],
+		references: [productVariants.id]
+	}),
+}));
+
+export const orderItemsRelations = relations(orderItems, ({one}) => ({
+	order: one(orders, {
+		fields: [orderItems.orderId],
+		references: [orders.id]
+	}),
+}));
+
+export const ordersRelations = relations(orders, ({many}) => ({
+	orderItems: many(orderItems),
+}));
+
+export const productCategoriesRelations = relations(productCategories, ({one}) => ({
+	category: one(categories, {
+		fields: [productCategories.categoryId],
+		references: [categories.id]
+	}),
+	product: one(products, {
+		fields: [productCategories.productId],
+		references: [products.id]
+	}),
+}));
+
+export const rolePermissionsRelations = relations(rolePermissions, ({one}) => ({
+	permission: one(permissions, {
+		fields: [rolePermissions.permissionId],
+		references: [permissions.id]
+	}),
+	role: one(roles, {
+		fields: [rolePermissions.roleId],
+		references: [roles.id]
+	}),
+}));
+
+export const permissionsRelations = relations(permissions, ({many}) => ({
+	rolePermissions: many(rolePermissions),
+}));
+
+export const rolesRelations = relations(roles, ({many}) => ({
+	rolePermissions: many(rolePermissions),
+}));
