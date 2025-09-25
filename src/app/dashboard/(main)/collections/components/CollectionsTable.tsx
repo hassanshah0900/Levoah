b73f8117438a1a03a6d5e3b4692f1f96ef18daea
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-25 14:34:53 +0500

    feat: create collections table

---
+++ src/app/dashboard/(main)/collections/components/CollectionsTable.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import DataTable from "@/components/DataTable/DataTable";
+import DataTableColumnVisibilityToggler from "@/components/DataTable/DataTableColumnVisibilityToggler";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { useDataTable } from "@/hooks/useDataTable";
+import { Plus } from "lucide-react";
+import Link from "next/link";
+import { Collection } from "../lib/types";
+import { columns } from "./columns";
+
+interface Props {
+  collections: Collection[];
+}
+export default function CollectionsTable({ collections }: Props) {
+  const { table } = useDataTable({
+    columns,
+    data: collections ?? [],
+    rowCount: 0,
+    initialState: {
+      columnPinning: {
+        right: ["actions"],
+      },
+    },
+  });
+
+  return (
+    <div className="space-y-5 mt-5 @container">
+      <div>
+        <SidebarTrigger />
+      </div>
+      <div className="flex justify-between items-start">
+        <div className="space-y-2 @lg:space-y-0">
+          <Input
+            className="max-w-xs"
+            placeholder="Search product titles..."
+            onChange={(e) => {
+              table.getColumn("title")?.setFilterValue(e.currentTarget.value);
+            }}
+          />
+          <DataTableColumnVisibilityToggler
+            table={table}
+            className="@lg:hidden"
+            align="start"
+          />
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <DataTableColumnVisibilityToggler
+            table={table}
+            className="hidden @lg:flex"
+            align="end"
+          />
+          <Link href={"/dashboard/collections/new"}>
+            <Button size={"sm"}>
+              <Plus /> New
+            </Button>
+          </Link>
+        </div>
+      </div>
+      <DataTable table={table}></DataTable>
+    </div>
+  );
+}
