d95f66a5212122f15f6d205a6af048f6cb3f105f
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-26 19:40:22 +0500

    fix: make it responsive as original grid

--- src/components/ProductsGridLoadingSkeleton.tsx
+++ src/components/ProductsGridLoadingSkeleton.tsx
@@ -1,5 +1,4 @@
 import ProductCardSkeleton from "@/app/(app)/[...categories]/components/ProductCardSkeleton";
-import React from "react";
 import { Skeleton } from "./ui/skeleton";
 
 export default function ProductsGridLoadingSkeleton() {
@@ -12,7 +11,7 @@ export default function ProductsGridLoadingSkeleton() {
           <Skeleton className="h-8 md:h-10 w-24 xs:w-40" />
         </div>
       </div>
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-x-5 gap-y-10">
+      <div className="grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] lg:grid-cols-4 gap-x-2 md:gap-x-4 gap-y-5 sm:gap-y-10">
         {Array.from({ length: 8 }).map((_, idx) => (
           <ProductCardSkeleton key={idx} />
         ))}
