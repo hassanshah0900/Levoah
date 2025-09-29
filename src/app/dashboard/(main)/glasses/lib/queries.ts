2155ee891b67b5fa114bee65a9c814c67a5ed147
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-29 21:01:58 +0500

    refactor: use drizzle to fetch glasses

--- src/app/dashboard/(main)/glasses/lib/queries.ts
+++ src/app/dashboard/(main)/glasses/lib/queries.ts
@@ -1,9 +1,9 @@
 "use server";
 
 import { db } from "@/db";
-import { productVariantsWithImages } from "@/db/drizzle/schema";
+import { glasses, productVariantsWithImages } from "@/db/drizzle/schema";
 import { createClient } from "@/supabase/server";
-import { ProductType, ProductVariant } from "@/types/products.types";
+import { Product, ProductType, ProductVariant } from "@/types/products.types";
 import { SortingState } from "@tanstack/react-table";
 import { desc, eq } from "drizzle-orm";
 import { Category } from "../../categories/lib/types";
@@ -12,34 +12,16 @@ interface GetAllProductsProps {
   filters: {
     pageIndex: number;
     pageSize: number;
-    title: string;
-    sorting: SortingState;
+    title?: string;
+    sorting?: SortingState;
   };
 }
 
-export async function getAllProducts({
-  filters: { pageIndex, pageSize, sorting, title },
-}: GetAllProductsProps) {
-  const supabase = await createClient();
-
-  let filterBuilder = supabase.from("glasses").select("*", { count: "exact" });
-
-  if (title) filterBuilder = filterBuilder.ilike("title", `%${title}%`);
-
-  for (let columnSort of sorting) {
-    filterBuilder = filterBuilder.order(columnSort.id, {
-      ascending: !columnSort.desc,
-    });
-  }
-
-  const rangeStart = pageIndex * pageSize;
-  const rangeEnd = rangeStart + pageSize - 1;
-  filterBuilder = filterBuilder.range(rangeStart, rangeEnd);
-
-  const { data, error, count } = await filterBuilder;
-
-  if (error) throw error;
-  return { glasses: data, count };
+export async function getAllProducts() {
+  return (await db
+    .select()
+    .from(glasses)
+    .orderBy(desc(glasses.createdAt))) as Product<"glasses">[];
 }
 
 export async function getAllGlassesVariants({
