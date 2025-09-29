765bbb66361a850faaa16329849c62f50ecc7318
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-29 21:17:11 +0500

    refactor: add loading indicator as global loading indicator

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,7 @@
 export default function loading() {
-  return <div>loading...</div>;
+  return (
+    <div className="w-full my-40 flex justify-center items-center">
+      <div className="w-20 h-20 rounded-full border-4 border-r-foreground animate-spin"></div>
+    </div>
+  );
 }
