31d08cf0cf4058e8c098234fd4ec22c2c781c847
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-29 21:45:18 +0500

    refactor: remove fetching in page file

--- src/app/dashboard/(main)/glasses/[productId]/page.tsx
+++ src/app/dashboard/(main)/glasses/[productId]/page.tsx
@@ -1,31 +1,10 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
 import GlassesVariantsTable from "../components/GlassesVariantsTable";
-import { getAllGlassesVariants } from "../lib/queries";
 
 export default async function DashboardProductPage({
   params,
 }: {
   params: Promise<{ productId: string }>;
 }) {
-  const resolvedParams = await params;
-  const productId = parseInt(resolvedParams.productId);
-
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ["product_variants", productId],
-    queryFn: () => getAllGlassesVariants({ productId }),
-  });
-
-  const state = dehydrate(queryClient);
-
-  return (
-    <HydrationBoundary state={state}>
-      <GlassesVariantsTable productId={productId} />
-    </HydrationBoundary>
-  );
+  const { productId } = await params;
+  return <GlassesVariantsTable productId={parseInt(productId)} />;
 }
