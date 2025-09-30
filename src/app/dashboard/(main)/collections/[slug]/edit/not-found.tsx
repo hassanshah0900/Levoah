8f813d16efb9108e751799fdf7f01f74c0930823
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-09-30 17:33:58 +0500

    feat: create not found page for collection

---
+++ src/app/dashboard/(main)/collections/[slug]/edit/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center mt-10">
+      <h1 className="text-2xl font-semibold">No such collection exits.</h1>
+    </div>
+  );
+}
