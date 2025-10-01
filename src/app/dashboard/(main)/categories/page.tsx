f5a3f86ec4c02370d22ba1b0ef73fc4650717969
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-10-01 17:02:00 +0500

    fix: remove prefetching in the page file

--- src/app/dashboard/(main)/categories/page.tsx
+++ src/app/dashboard/(main)/categories/page.tsx
@@ -1,24 +1,5 @@
-import React from "react";
 import CategoriesTable from "./components/CategoriesTable";
-import { getAllCategories } from "./lib/queries";
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
 
 export default async function CategoriesPage() {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ["categories"],
-    queryFn: getAllCategories,
-  });
-
-  const state = dehydrate(queryClient);
-  return (
-    <HydrationBoundary state={state}>
-      <CategoriesTable />
-    </HydrationBoundary>
-  );
+  return <CategoriesTable />;
 }
