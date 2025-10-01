a08accc7a2d31e7e7c944df539ed374650733bcb
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-10-01 17:20:20 +0500

    fix: make categories page static

--- src/app/dashboard/(main)/categories/page.tsx
+++ src/app/dashboard/(main)/categories/page.tsx
@@ -1,5 +1,5 @@
 import CategoriesTable from "./components/CategoriesTable";
 
-export default async function CategoriesPage() {
+export default function CategoriesPage() {
   return <CategoriesTable />;
 }
