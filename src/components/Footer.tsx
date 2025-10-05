479196c2e03b6f3e941899bb909386361be418fd
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-10-05 19:01:17 +0500

    style: change footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
+import Image from "next/image";
 import Link from "next/link";
 import { FaFacebookF, FaTiktok } from "react-icons/fa6";
 import { RiInstagramFill } from "react-icons/ri";
+import logo from "../../public/images/Levoah.png";
 import Container from "./Container";
 
 const links = [
@@ -11,11 +13,12 @@ const links = [
 ];
 export default function Footer() {
   return (
-    <footer className="bg-primary pt-10 pb-20 border-t-2 border-highlight/60">
+    <footer className="bg-footer/70 text-footer-foreground backdrop-blur-sm pt-10 pb-20 shadow-black/40 shadow-[0_0_10px]">
       <Container>
         <div className="grid gird-cols-1 gap-8 xs:gap-x-4 xs:grid-cols-[2fr_1fr] md:gap-4 md:grid-cols-[2fr_repeat(3,1fr)]">
           <div className="space-y-5">
-            <p className="text-background">
+            <Image src={logo} alt="Levoah Logo" className="h-16 w-auto" />
+            <p className="">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor
               temporibus rerum saepe reiciendis dolore, modi voluptatem
               asperiores ipsam placeat cupiditate corporis omnis sequi
@@ -23,19 +26,19 @@ export default function Footer() {
             <div className="flex items-center gap-4">
               <Link
                 href={"https://www.instagram.com/levoahfashion"}
-                className="text-primary-foreground text-2xl"
+                className="text-2xl"
               >
                 <RiInstagramFill />
               </Link>
               <Link
                 href={"https://www.facebook.com/share/1A4JMP5bxR/"}
-                className="text-primary-foreground text-xl"
+                className="text-xl"
               >
                 <FaFacebookF />
               </Link>
               <Link
                 href={"https://www.tiktok.com/@levoahfashion"}
-                className="text-primary-foreground text-xl"
+                className="text-xl"
               >
                 <FaTiktok />
               </Link>
@@ -43,42 +46,30 @@ export default function Footer() {
           </div>
           <div>
             <ul className="space-y-1.5">
-              <h4 className="text-lg font-semibold text-primary-foreground mb-3">
-                Heading
-              </h4>
+              <h4 className="text-lg font-semibold mb-3">Heading</h4>
               {links.map(({ label }) => (
                 <li key={label}>
-                  <Link href={""} className="text-background">
-                    {label}
-                  </Link>
+                  <Link href={""}>{label}</Link>
                 </li>
               ))}
             </ul>
           </div>
           <div>
             <ul className="space-y-1.5">
-              <h4 className="text-lg font-semibold text-primary-foreground mb-3">
-                Heading
-              </h4>
+              <h4 className="text-lg font-semibold mb-3">Heading</h4>
               {links.map(({ label }) => (
                 <li key={label}>
-                  <Link href={""} className="text-background">
-                    {label}
-                  </Link>
+                  <Link href={""}>{label}</Link>
                 </li>
               ))}
             </ul>
           </div>
           <div>
             <ul className="space-y-1.5">
-              <h4 className="text-lg font-semibold text-primary-foreground mb-3">
-                Heading
-              </h4>
+              <h4 className="text-lg font-semibold mb-3">Heading</h4>
               {links.map(({ label }) => (
                 <li key={label}>
-                  <Link href={""} className="text-background">
-                    {label}
-                  </Link>
+                  <Link href={""}>{label}</Link>
                 </li>
               ))}
             </ul>
