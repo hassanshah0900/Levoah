ae030d148337b6cc999e0a809f39ee5cf5acaf60
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-10-06 16:34:25 +0500

    feat: create data for navbar

---
+++ src/lib/navbar-data.ts
@@ -0,0 +1,41 @@
+export type SubLink = {
+  imageUrl?: string;
+  title: string;
+  url: string;
+};
+
+export const sunglassesSublinks: SubLink[] = [
+  {
+    imageUrl: "/images/men.webp",
+    title: "Men",
+    url: "/collections/sunglasses-men",
+  },
+  {
+    imageUrl: "/images/women.webp",
+    title: "Women",
+    url: "/collections/sunglasses-women",
+  },
+  {
+    imageUrl: "/images/child.webp",
+    title: "Children",
+    url: "/collections/sunglasses-children",
+  },
+];
+
+export const eyeglassesSublinks: SubLink[] = [
+  {
+    imageUrl: "/images/men.webp",
+    title: "Men",
+    url: "/collections/eyeglasses-men",
+  },
+  {
+    imageUrl: "/images/women.webp",
+    title: "Women",
+    url: "/collections/eyeglasses-women",
+  },
+  {
+    imageUrl: "/images/child.webp",
+    title: "Children",
+    url: "/collections/eyeglasses-children",
+  },
+];
