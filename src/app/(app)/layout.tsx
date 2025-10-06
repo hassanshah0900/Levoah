1ecbd0d2f76d989b7ae3e8861df17e460e759fa6
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-10-06 20:22:32 +0500

    chore: hide shopping cart and all navigation links on checkout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,17 +1,25 @@
+"use client";
+
 import FloatingShoppingCart from "@/components/FloatingShoppingCart";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { ShoppingCartProvider } from "@/contexts/ShoppingCartContext";
+import { usePathname } from "next/navigation";
 import { PropsWithChildren } from "react";
 
 export default function layout({ children }: PropsWithChildren) {
+  const pathname = usePathname();
   return (
     <ShoppingCartProvider>
       <div>
         <Navbar />
         {children}
-        <FloatingShoppingCart />
-        <Footer />
+        {pathname !== "/checkout" && (
+          <>
+            <FloatingShoppingCart />
+            <Footer />
+          </>
+        )}
       </div>
     </ShoppingCartProvider>
   );
