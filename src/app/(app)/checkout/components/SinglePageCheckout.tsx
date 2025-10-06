fee35488c287380756105b62f08c636d0cbafcaa
Syed Muhammad Hassan Shah <hasni9267@gmail.com>
2025-10-06 20:28:36 +0500

    feat: use react query to create order

--- src/app/(app)/checkout/components/SinglePageCheckout.tsx
+++ src/app/(app)/checkout/components/SinglePageCheckout.tsx
@@ -3,8 +3,13 @@
 import { Button } from "@/components/ui/button";
 import { Form } from "@/components/ui/form";
 import { Separator } from "@/components/ui/separator";
+import { useShoppingCart } from "@/contexts/ShoppingCartContext";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { createOrder } from "../lib/actions";
 import { checkoutFormSchema } from "../lib/validation";
 import CustomerAddressFields from "./CustomerAddressFields";
 import OrderSummary from "./OrderSummary";
@@ -12,37 +17,52 @@ import PaymentOptions from "./PaymentOptions";
 import ReviewOrder from "./ReviewOrder";
 
 export default function SinglePageCheckout() {
+  const { reset } = useShoppingCart();
+  const router = useRouter();
+  const { mutate, isPending } = useMutation({
+    mutationFn: createOrder,
+    onSuccess({ orderTrackingCode }) {
+      toast.success("Your order has been successfully placed.", {
+        id: "create order",
+      });
+      router.push(`/order/confirmation/${orderTrackingCode}`);
+      reset();
+    },
+    onError() {
+      toast.error("Couldn't place order.", { id: "create order" });
+    },
+  });
   const form = useForm({
     resolver: zodResolver(checkoutFormSchema),
     mode: "onChange",
     defaultValues: {
-      shipping_address: {
-        full_name: "",
+      shippingAddress: {
+        fullName: "",
         address: "",
         city: "",
         country: "Pakistan",
         email: "",
         phone: "",
         province: "",
-        postal_code: "",
+        postalCode: "",
       },
-      payment_method: "Cash on delivery",
+      paymentMethod: "BANKTRANSFER",
     },
   });
   function onSubmit(data: any) {
-    console.log(data);
+    toast.loading("Creating order...", { id: "create order" });
+    mutate(data);
   }
 
   return (
     <Form {...form}>
       <form
-        action=""
-        className="grid grid-cols-2 gap-5"
+        className="grid grid-cols-1 md:grid-cols-2 gap-5"
         onSubmit={form.handleSubmit(onSubmit)}
       >
         <div className="space-y-5">
           <ReviewOrder />
-          <Separator />
+          <Separator className="md:mb-0" />
           <CustomerAddressFields />
         </div>
         <div className="space-y-5">
@@ -50,7 +70,7 @@ export default function SinglePageCheckout() {
           <PaymentOptions />
           <Separator />
           <OrderSummary />
-          <Button className="w-full" type="submit">
+          <Button className="w-full" type="submit" disabled={isPending}>
             Complete Order
           </Button>
         </div>
